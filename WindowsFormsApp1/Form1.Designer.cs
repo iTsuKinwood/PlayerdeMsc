09adb8abf18692578dbcb52e86c54cd5d34d88ee
RM20232930126 <RM20232930126@LAB6-18>
2023-11-08 20:24:16 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Player
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Player));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.op = new System.Windows.Forms.ListBox();
+            this.abrir = new System.Windows.Forms.Button();
+            this.ofdAbrir = new System.Windows.Forms.OpenFileDialog();
+            this.sfdSalvar = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(470, 317);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // op
+            // 
+            this.op.FormattingEnabled = true;
+            this.op.Location = new System.Drawing.Point(12, 335);
+            this.op.Name = "op";
+            this.op.Size = new System.Drawing.Size(470, 160);
+            this.op.TabIndex = 1;
+            // 
+            // abrir
+            // 
+            this.abrir.Location = new System.Drawing.Point(12, 501);
+            this.abrir.Name = "abrir";
+            this.abrir.Size = new System.Drawing.Size(75, 23);
+            this.abrir.TabIndex = 0;
+            this.abrir.Text = "Abrir";
+            this.abrir.UseVisualStyleBackColor = true;
+            this.abrir.Click += new System.EventHandler(this.abrir_Click);
+            // 
+            // Player
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 570);
+            this.Controls.Add(this.abrir);
+            this.Controls.Add(this.op);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Player";
+            this.Text = "Player";
+            this.Load += new System.EventHandler(this.Player_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListBox op;
+        private System.Windows.Forms.Button abrir;
+        private System.Windows.Forms.OpenFileDialog ofdAbrir;
+        private System.Windows.Forms.SaveFileDialog sfdSalvar;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Player : Form
+    {
+        public Player()
+        {
+            InitializeComponent();
+        }
+
+        WMPLib.IWMPPlaylist playlist;
+        private void Player_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void abrir_Click(object sender, EventArgs e)
+        {
+            ofdAbrir.Title = "Abrir mídia";
+            ofdAbrir.Filter = "Arquivo mp4|*.mp4|Arquivo mp3|*.mp3";
+            if (ofdAbrir.ShowDialog() == DialogResult.OK)
+            {
+                playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist("Lista");
+
+                foreach (var arquivo in ofdAbrir.FileNames)
+                {
+                    playlist.appendItem(axWindowsMediaPlayer1.newMedia(arquivo));
+                    op.Items.Add(arquivo);
+
+
+                    axWindowsMediaPlayer1.currentPlaylist = playlist;
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                }
+            }
+        }
+    }
+}
+
+
+   
+
